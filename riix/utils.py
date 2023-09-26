cc831d794b14ce2b9c9be6fe8819926594225dc0
cthorrez <claytonthorrez@gmail.com>
2023-09-25 21:02:57 -0700

    reset for v2 from scratch

--- riix/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def sigmoid(x, k=1):
-    return 1 / (1 + np.exp(-x*k))
-
-def get_num_competitors(df, competitor_cols):
-    all_cols_unique = np.concatenate([df[col].unique() for col in competitor_cols])
-    return len(set(all_cols_unique))
