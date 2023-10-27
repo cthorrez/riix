f576367d8a71b30d2cf7a98cb5ef999774ec7caa
cthorrez <claytonthorrez@gmail.com>
2023-10-26 22:51:11 -0700

    add binary_log_loss

--- riix/metrics.py
+++ riix/metrics.py
@@ -3,13 +3,13 @@
 import numpy as np
 
 
-def binary_accuracy(probs: np.ndarray, outcomes: np.ndarray):
+def binary_accuracy(probs: np.ndarray, outcomes: np.ndarray) -> float:
     """compute accuracy where outcomes is binary and ties are broken in favor of positive class"""
     preds = probs >= 0.5
-    return (preds == probs).mean()
+    return (preds == outcomes).mean()
 
 
-def accuracy_with_draws(probs: np.ndarray, outcomes: np.ndarray, draw_margin=0.0):
+def accuracy_with_draws(probs: np.ndarray, outcomes: np.ndarray, draw_margin=0.0) -> float:
     """computes accuracy while allowing for ties"""
     pos_pred_mask = probs > (0.5 + draw_margin)
     neg_pred_mask = probs < (0.5 - draw_margin)
@@ -18,3 +18,9 @@ def accuracy_with_draws(probs: np.ndarray, outcomes: np.ndarray, draw_margin=0.0
     correct += (1.0 - outcomes)[neg_pred_mask].sum()
     correct += 2 * outcomes(draw_pred_mask).sum()  # lmao
     return correct / outcomes.shape[0]
+
+
+def binary_log_loss(probs: np.ndarray, outcomes: np.ndarray) -> float:
+    """compute log loss where outcome is binary 1.0 or 0.0"""
+    loss_array = (np.log(probs) * outcomes) + (np.log(1.0 - probs) * (1.0 - outcomes))
+    return loss_array.mean()
