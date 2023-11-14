1be67487bfe4c68c1a781ef73889d0999ef8d83d
cthorrez <claytonthorrez@gmail.com>
2023-11-13 23:54:58 -0800

    make a little more progress

--- riix/models/trueskill.py
+++ riix/models/trueskill.py
@@ -1,7 +1,7 @@
 """TrueSkill"""
 import numpy as np
 from scipy.stats import norm
-from riix.core.base import Onlinemusystem
+from riix.core.base import OnlineRatingSystem
 
 
 def v_and_w(t, eps):
@@ -12,28 +12,27 @@ def v_and_w(t, eps):
     return v, w
 
 
-class TrueSkill(Onlinemusystem):
+class TrueSkill(OnlineRatingSystem):
     """the og TrueSkill rating system shoutout to Microsoft"""
 
     def __init__(
         self,
         num_competitors: int,
-        initial_rating: float = 25.0,
-        initial_rating_dev: float = 8.333,
+        initial_mu: float = 25.0,
+        initial_sigma: float = 8.333,
         beta: float = 4.166,
         tau: float = 0.0833,
         epsilon: float = 0.0,
         dtype=np.float64,
     ):
         self.num_competitors = num_competitors
-        self.initial_rating_dev = initial_rating_dev
         self.beta = beta
         self.two_beta_squared = 2.0 * (beta**2.0)
         self.tau = tau
         self.tau_squared = tau**2.0
         self.epsilon = epsilon
-        self.mus = np.zeros(shape=num_competitors, dtype=dtype) + initial_rating
-        self.sigma2s = np.zeros(shape=num_competitors, dtype=dtype) + initial_rating_dev**2.0
+        self.mus = np.zeros(shape=num_competitors, dtype=dtype) + initial_mu
+        self.sigma2s = np.zeros(shape=num_competitors, dtype=dtype) + initial_sigma**2.0
         self.has_played = np.zeros(shape=num_competitors, dtype=np.bool_)
         self.cache = {'combined_devs': None, 'norm_diffs': None}
 
@@ -43,7 +42,7 @@ class TrueSkill(Onlinemusystem):
         mus_2 = self.mus[matchups[:, 1]]
         sigma2s_1 = self.sigma2s[matchups[:, 0]]
         sigma2s_2 = self.sigma2s[matchups[:, 1]]
-        combined_sigma2s = self.two_beta_squared * sigma2s_1 + sigma2s_2
+        combined_sigma2s = self.two_beta_squared + sigma2s_1 + sigma2s_2
         combined_devs = np.sqrt(combined_sigma2s)
         norm_diffs = (mus_1 - mus_2) / combined_devs
         if set_cache:
@@ -70,7 +69,7 @@ class TrueSkill(Onlinemusystem):
         """called once per period to model the increase in variance over time"""
         active_in_period = np.unique(matchups)
         self.has_played[active_in_period] = True
-        self.sigma2s[self.has_played] + +self.tau_squared
+        self.sigma2s[self.has_played] += self.tau_squared
         return active_in_period
 
     def batched_update(self, matchups, outcomes, use_cache=False):
@@ -90,12 +89,20 @@ class TrueSkill(Onlinemusystem):
             combined_devs = np.sqrt(combined_sigma2s)
             norm_diffs = (rating_diffs) / combined_devs
 
-        vs, ws = v_and_w(norm_diffs, self.epsilon / combined_devs)
+        # map 1 to 1, 0 to -1, 0.5 to 0.5
+        outcome_multiplier = outcomes.copy()
+        outcome_multiplier[outcome_multiplier == 0] = -1
+        outcome_multiplier = np.hstack([outcome_multiplier[:, None], -1 * outcome_multiplier[:, None]])
 
-        mu_updates = vs * (sigma2s / combined_devs)
-        sigma2_updates = ws * sigma2s / combined_sigma2s
+        vs, ws = v_and_w(norm_diffs[:, None] * outcome_multiplier, (self.epsilon / combined_devs)[:, None])
 
-        print(mu_updates, sigma2_updates, masks)
+        mu_updates = vs[:, None] * (sigma2s / combined_devs[:, None])
+        sigma2_updates = ws[:, None] * sigma2s / combined_sigma2s[:, None]
+        mu_updates_pooled = (mu_updates[:, :, None] * masks).sum((0, 1))
+        sigma2_updates_pooled = (sigma2_updates[:, :, None] * masks).sum((0, 1))
+
+        self.mus[active_in_period] += mu_updates_pooled
+        self.sigma2s[active_in_period] -= sigma2_updates_pooled
 
     def iterative_update(self, matchups, outcomes):
         """treat the matchups in the rating period as if they were sequential"""
