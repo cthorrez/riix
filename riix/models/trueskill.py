b2f4db049b6d2b67623901fa0d0d56027122d875
cthorrez <claytonthorrez@gmail.com>
2023-11-14 22:45:41 -0800

    make trueskill better

--- riix/models/trueskill.py
+++ riix/models/trueskill.py
@@ -5,9 +5,25 @@ from riix.core.base import OnlineRatingSystem
 
 
 def v_and_w(t, eps):
-    """calculate v and w in a pretty efficient way"""
+    """calculate v and w for a win"""
     diff = t - eps
     v = norm.pdf(diff) / norm.cdf(diff)
+
+    bad_mask = np.isnan(v) | np.isinf(v)
+    if bad_mask.any():
+        v[bad_mask] = (-1 * (diff))[bad_mask]
+    w = v * (v + diff)
+    return v, w
+
+
+def v_and_w_draw(t, eps):
+    """calculate v and w for a win"""
+    diff = t - eps
+    v = norm.pdf(diff) / norm.cdf(diff)
+
+    bad_mask = np.isnan(v) | np.isinf(v)
+    if bad_mask.any():
+        v[bad_mask] = (-1 * (diff))[bad_mask]
     w = v * (v + diff)
     return v, w
 
@@ -28,7 +44,6 @@ class TrueSkill(OnlineRatingSystem):
         self.num_competitors = num_competitors
         self.beta = beta
         self.two_beta_squared = 2.0 * (beta**2.0)
-        self.tau = tau
         self.tau_squared = tau**2.0
         self.epsilon = epsilon
         self.mus = np.zeros(shape=num_competitors, dtype=dtype) + initial_mu
@@ -38,13 +53,11 @@ class TrueSkill(OnlineRatingSystem):
 
     def predict(self, time_step: int, matchups: np.ndarray, set_cache: bool = False):
         """generate predictions"""
-        mus_1 = self.mus[matchups[:, 0]]
-        mus_2 = self.mus[matchups[:, 1]]
-        sigma2s_1 = self.sigma2s[matchups[:, 0]]
-        sigma2s_2 = self.sigma2s[matchups[:, 1]]
-        combined_sigma2s = self.two_beta_squared + sigma2s_1 + sigma2s_2
+        mus = self.mus[matchups]
+        sigma2s = self.sigma2s[matchups]
+        combined_sigma2s = self.two_beta_squared + sigma2s.sum(axis=1)
         combined_devs = np.sqrt(combined_sigma2s)
-        norm_diffs = (mus_1 - mus_2) / combined_devs
+        norm_diffs = (mus[:, 0] - mus[:, 1]) / combined_devs
         if set_cache:
             self.cache['norm_diffs'] = norm_diffs
             self.cache['combined_sigma2s'] = combined_sigma2s
@@ -58,7 +71,7 @@ class TrueSkill(OnlineRatingSystem):
         matchups: np.ndarray,
         outcomes: np.ndarray,
         use_cache: bool = False,
-        update_method: str = 'batched',
+        update_method: str = 'iterative',
     ):
         if update_method == 'batched':
             self.batched_update(matchups, outcomes, use_cache)
@@ -91,15 +104,19 @@ class TrueSkill(OnlineRatingSystem):
 
         # map 1 to 1, 0 to -1, 0.5 to 0.5
         outcome_multiplier = outcomes.copy()
-        outcome_multiplier[outcome_multiplier == 0] = -1
-        outcome_multiplier = np.hstack([outcome_multiplier[:, None], -1 * outcome_multiplier[:, None]])
+        outcome_multiplier[outcome_multiplier == 0] = -1.0
 
-        vs, ws = v_and_w(norm_diffs[:, None] * outcome_multiplier, (self.epsilon / combined_devs)[:, None])
+        vs, ws = v_and_w(norm_diffs * outcome_multiplier, self.epsilon / combined_devs)
 
         mu_updates = vs[:, None] * (sigma2s / combined_devs[:, None])
-        sigma2_updates = ws[:, None] * sigma2s / combined_sigma2s[:, None]
+
+        # map it back?
+        mu_updates[:, 1] *= -1.0
+        mu_updates = mu_updates * outcome_multiplier[:, None]
+
+        sigma2_updates = (np.square(sigma2s) * ws[:, None]) / combined_sigma2s[:, None]
         mu_updates_pooled = (mu_updates[:, :, None] * masks).sum((0, 1))
-        sigma2_updates_pooled = (sigma2_updates[:, :, None] * masks).sum((0, 1))
+        sigma2_updates_pooled = (sigma2_updates[:, :, None] * masks).mean((0, 1))
 
         self.mus[active_in_period] += mu_updates_pooled
         self.sigma2s[active_in_period] -= sigma2_updates_pooled
@@ -110,4 +127,20 @@ class TrueSkill(OnlineRatingSystem):
         for idx in range(matchups.shape[0]):
             comp_1, comp_2 = matchups[idx]
             rating_diff = self.mus[comp_1] - self.mus[comp_2]
-            print(rating_diff)
+            sigma2s = self.sigma2s[matchups[idx]]
+
+            combined_sigma2 = self.two_beta_squared + sigma2s.sum()
+            combined_dev = np.sqrt(combined_sigma2)
+            norm_diff = (rating_diff) / combined_dev
+
+            outcome = outcomes[idx]
+            outcome_multiplier = outcome if outcome else -1.0
+            v, w = v_and_w(norm_diff * outcome_multiplier, self.epsilon / combined_dev)
+
+            sign_multiplier = 1.0 if outcome == 1 else -1
+            mu_updates = (sigma2s / combined_dev) * v * sign_multiplier
+
+            sigma2_updates = (np.square(sigma2s) / combined_sigma2) * w
+            self.mus[comp_1] += mu_updates[0]
+            self.mus[comp_2] -= mu_updates[1]
+            self.sigma2s[matchups[idx]] -= sigma2_updates
