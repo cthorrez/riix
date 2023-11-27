db3ebd61be2b74ef6c7df2bdaf8801719c727902
cthorrez <claytonthorrez@gmail.com>
2023-11-26 23:37:06 -0500

    add wl tm iterative

--- riix/models/weng_lin_thurstone_mosteller.py
+++ riix/models/weng_lin_thurstone_mosteller.py
@@ -1,13 +1,13 @@
-"""TrueSkill"""
+"""Weng/Lin Bayesian Online Rating system, Thurstone Mosteller Edition"""
 import math
 import numpy as np
 from scipy.stats import norm
 from riix.core.base import OnlineRatingSystem
-from riix.utils.math_utils import v_and_w_win_scalar, v_and_w_win_vector, v_and_w_draw_vector, v_and_w_draw_scalar
+from riix.utils.math_utils import v_and_w_win_scalar, v_and_w_draw_scalar
 
 
-class TrueSkill(OnlineRatingSystem):
-    """the og TrueSkill rating system shoutout to Microsoft"""
+class WengLinThurstoneMosteller(OnlineRatingSystem):
+    """The Bayesian Online Rating System introduced by Weng and Lin"""
 
     def __init__(
         self,
@@ -15,6 +15,7 @@ class TrueSkill(OnlineRatingSystem):
         initial_mu: float = 25.0,
         initial_sigma: float = 8.333,
         beta: float = 4.166,
+        kappa: float = 0.0001,
         tau: float = 0.0833,
         draw_probability=0.0,
         update_method: str = 'iterative',
@@ -22,14 +23,16 @@ class TrueSkill(OnlineRatingSystem):
     ):
         self.num_competitors = num_competitors
         self.beta = beta
+        self.kappa = kappa
         self.two_beta_squared = 2.0 * (beta**2.0)
         self.tau_squared = tau**2.0
-
         self.epsilon = norm.ppf((draw_probability + 1.0) / 2.0) * math.sqrt(2.0) * beta
+
         self.mus = np.zeros(shape=num_competitors, dtype=dtype) + initial_mu
         self.sigma2s = np.zeros(shape=num_competitors, dtype=dtype) + initial_sigma**2.0
         self.has_played = np.zeros(shape=num_competitors, dtype=np.bool_)
-        self.cache = {'combined_devs': None, 'norm_diffs': None}
+        self.cache = {'TODO': None}
+
         if update_method == 'batched':
             self.update_fn = self.batched_update
         elif update_method == 'iterative':
@@ -42,10 +45,6 @@ class TrueSkill(OnlineRatingSystem):
         combined_sigma2s = self.two_beta_squared + sigma2s.sum(axis=1)
         combined_devs = np.sqrt(combined_sigma2s)
         norm_diffs = (mus[:, 0] - mus[:, 1]) / combined_devs
-        if set_cache:
-            self.cache['norm_diffs'] = norm_diffs
-            self.cache['combined_sigma2s'] = combined_sigma2s
-            self.cache['combined_devs'] = combined_devs
         probs = norm.cdf(norm_diffs)
         return probs
 
@@ -62,57 +61,13 @@ class TrueSkill(OnlineRatingSystem):
         """called once per period to model the increase in variance over time"""
         active_in_period = np.unique(matchups)
         self.has_played[active_in_period] = True
-        self.sigma2s[active_in_period] += self.tau_squared  # increase var for currently playing players
-        # self.sigma2s[self.has_played] += self.tau_squared  # increase var for ALL players
+        # self.sigma2s[active_in_period] += self.tau_squared  # increase var for currently playing players
+        self.sigma2s[self.has_played] += self.tau_squared  # increase var for ALL players
         return active_in_period
 
     def batched_update(self, matchups, outcomes, use_cache=False):
         """apply one update based on all of the results of the rating period"""
-        active_in_period = self.increase_rating_dev(matchups)
-        masks = np.equal(matchups[:, :, None], active_in_period[None, :])  # N x 2 x active
-
-        sigma2s = self.sigma2s[matchups]
-        if use_cache:
-            norm_diffs = self.cache['norm_diffs']
-            combined_devs = self.cache['combined_devs']
-            combined_sigma2s = self.cache['combined_sigma2s']
-        else:
-            mus = self.mus[matchups]
-            rating_diffs = mus[:, 0] - mus[:, 1]
-            combined_sigma2s = self.two_beta_squared + sigma2s.sum(axis=1)
-            combined_devs = np.sqrt(combined_sigma2s)
-            norm_diffs = (rating_diffs) / combined_devs
-
-        # map 1 to 1, 0 to -1, 0.5 to 0.5
-        outcome_multiplier = outcomes.copy()
-        outcome_multiplier[outcome_multiplier == 0] = -1.0
-
-        vs = np.empty_like(norm_diffs)
-        ws = np.empty_like(norm_diffs)
-        win_mask = outcomes != 0.5
-        draw_mask = ~win_mask
-
-        vs[win_mask], ws[win_mask] = v_and_w_win_vector(
-            norm_diffs[win_mask] * outcome_multiplier[win_mask], self.epsilon / combined_devs[win_mask]
-        )
-        vs[draw_mask], ws[draw_mask] = v_and_w_draw_vector(
-            norm_diffs[draw_mask] * outcome_multiplier[draw_mask], self.epsilon / combined_devs[draw_mask]
-        )
-
-        # vs, ws = v_and_w_win_vector(norm_diffs * outcome_multiplier, self.epsilon / combined_devs)
-
-        mu_updates = vs[:, None] * (sigma2s / combined_devs[:, None])
-
-        # map it back?
-        mu_updates[:, 1] *= -1.0
-        mu_updates = mu_updates * outcome_multiplier[:, None]
-
-        sigma2_updates = (np.square(sigma2s) * ws[:, None]) / combined_sigma2s[:, None]
-        mu_updates_pooled = (mu_updates[:, :, None] * masks).sum((0, 1))
-        sigma2_updates_pooled = (sigma2_updates[:, :, None] * masks).mean((0, 1))
-
-        self.mus[active_in_period] += mu_updates_pooled
-        self.sigma2s[active_in_period] -= sigma2_updates_pooled
+        pass
 
     def iterative_update(self, matchups, outcomes, **kwargs):
         """treat the matchups in the rating period as if they were sequential"""
@@ -133,8 +88,12 @@ class TrueSkill(OnlineRatingSystem):
             else:
                 v, w = v_and_w_draw_scalar(norm_diff, self.epsilon / combined_dev)
 
-            mu_updates = (sigma2s / combined_dev) * v * sign_multiplier
-            sigma2_updates = (np.square(sigma2s) / combined_sigma2) * w
-            self.mus[comp_1] += mu_updates[0]
-            self.mus[comp_2] -= mu_updates[1]
-            self.sigma2s[matchups[idx]] -= sigma2_updates
+            deltas = sign_multiplier * (sigma2s / combined_dev) * v
+
+            gammas = np.sqrt(sigma2s) / combined_dev
+            etas = gammas * (sigma2s / combined_sigma2) * w
+            sigma2_multipliers = np.maximum(1.0 - etas, self.kappa)
+
+            self.mus[comp_1] += deltas[0]
+            self.mus[comp_2] -= deltas[1]
+            self.sigma2s[matchups[idx]] *= sigma2_multipliers
