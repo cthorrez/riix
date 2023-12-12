126c5ebe452f9565c93240466fc929ec694915dd
cthorrez <claytonthorrez@gmail.com>
2023-12-11 19:12:03 -0800

    add batch_size param to dataset

--- riix/utils/data_utils.py
+++ riix/utils/data_utils.py
@@ -1,4 +1,5 @@
 """classes and functions for working with rating data"""
+import math
 from typing import List
 import numpy as np
 import pandas as pd
@@ -15,11 +16,14 @@ class RatingDataset:
         datetime_col: str = None,
         timestamp_col: str = None,
         rating_period: str = '1W',
+        batch_size: int = None,
     ):
         if len(competitor_cols) != 2:
             raise ValueError('must specify exactly 2 competitor columns')
         if (bool(datetime_col) + bool(timestamp_col)) != 1:
             raise ValueError('must specify only one of datetime_col and timestamp_col')
+
+        self.batch_size = batch_size
 
         if datetime_col:
             # get integer time_steps starting from 0 and increasing one per rating period
@@ -46,9 +50,17 @@ class RatingDataset:
         print('loaded dataset with:')
         print(f'{self.matchups.shape[0]} matchups')
         print(f'{len(self.idx_to_competitor)} unique competitors')
-        print(f'{np.max(self.time_steps)} rating periods of length {rating_period}')
 
-    def __iter__(self):
+        if batch_size is not None:
+            self.iter_fn = self.iter_by_batch
+            self.num_batches = math.ceil(len(self) / self.batch_size)
+            print(f'{self.num_batches} batches of length {batch_size}')
+        else:
+            self.iter_fn = self.iter_by_rating_period
+            print(f'{np.max(self.time_steps)} rating periods of length {rating_period}')
+
+    def iter_by_rating_period(self):
+        """iterate batches one rating period at a time"""
         period_start_idx = 0
         for period_end_idx in self.end_time_step_idxs:
             time_step = self.time_steps[period_start_idx]
@@ -56,6 +68,22 @@ class RatingDataset:
             outcomes = self.outcomes[period_start_idx:period_end_idx]
             period_start_idx = period_end_idx
             yield time_step, matchups, outcomes
+
+    def iter_by_batch(self, batch_size=None):
+        """iterate in fixed size batches"""
+        num_batches = math.ceil(len(self) / self.batch_size)
+        batch_start_idx = 0
+        for _ in range(num_batches):
+            batch_end_idx = batch_start_idx + self.batch_size
+            time_step = self.time_steps[batch_start_idx]
+            matchups = self.matchups[batch_start_idx:batch_end_idx]
+            outcomes = self.outcomes[batch_start_idx:batch_end_idx]
+            batch_start_idx = batch_end_idx
+            yield time_step, matchups, outcomes
+
+    def __iter__(self):
+        for tup in iter(self.iter_fn()):
+            yield tup
 
     def __len__(self):
         return self.matchups.shape[0]
