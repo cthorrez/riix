ecbea556996a8c8bd1888b5e9e74d57c6892f258
cthorrez <claytonthorrez@gmail.com>
2024-01-03 21:42:47 -0800

    add temporal massey

--- riix/models/temporal_massey.py
+++ riix/models/temporal_massey.py
@@ -1,30 +1,56 @@
 """
-Iterative Markov Method
-https://www.degruyter.com/document/doi/10.1515/jqas-2019-0070
+Temporalized Massey Method
+https://www.degruyter.com/document/doi/10.1515/jqas-2016-0093
+https://arxiv.org/abs/1702.00585
+
 """
 import numpy as np
 from riix.core.base import OnlineRatingSystem
 
 
-class IterativeMarkov(OnlineRatingSystem):
+class TemporalMassey(OnlineRatingSystem):
     def __init__(
         self,
         num_competitors: int,
         initial_rating: float = 1.0,
-        c: float = 0.1,
-        weight_with_prob: bool = False,  # if True this becomes "Linear Elo"
+        alpha: float = 0.95,
+        beta: float = 0.05,
         update_method: str = 'iterative',
         dtype=np.float64,
     ):
         self.num_competitors = num_competitors
-        self.c = c
-        self.weight_with_prob = weight_with_prob
         self.ratings = np.zeros(shape=num_competitors, dtype=dtype) + initial_rating
+        if (alpha is not None) and (beta is not None):
+            self.alpha = alpha
+            self.beta = beta
+            self.get_coefs = self.get_constant_coefs
+        else:
+            self.get_coefs = self.get_varying_coefs
+            self.matchup_counts = np.zeros(shape=num_competitors, dtype=dtype)
+
         self.cache = {'probs': None}
         if update_method == 'batched':
             self.update_fn = self.batched_update
         elif update_method == 'iterative':
             self.update_fn = self.iterative_update
+
+    def get_constant_coefs(self, **kwargs):
+        return self.alpha, self.beta, self.alpha, self.beta
+
+    def get_varying_coefs_helper(self, idx):
+        count = self.matchup_counts[idx]
+        if count == 0:
+            alpha, beta = 0.0, 0.0
+        else:
+            alpha = (count - 1.0) / count
+            beta = 1.0 / count
+        self.matchup_counts[idx] += 1
+        return alpha, beta
+
+    def get_varying_coefs(self, idx_1, idx_2):
+        alpha_1, beta_1 = self.get_varying_coefs_helper(idx_1)
+        alpha_2, beta_2 = self.get_varying_coefs_helper(idx_2)
+        return alpha_1, beta_1, alpha_2, beta_2
 
     def predict(self, time_step: int, matchups: np.ndarray, set_cache: bool = False):
         """generate predictions"""
@@ -54,13 +80,13 @@ class IterativeMarkov(OnlineRatingSystem):
             comp_1, comp_2 = matchups[idx]
             r_1 = self.ratings[comp_1]
             r_2 = self.ratings[comp_2]
-            r_sum = r_1 + r_2
-            prob_1 = r_1 / r_sum
-            update = self.c * r_sum * (outcomes[idx] - prob_1)
-            if self.weight_with_prob:
-                update *= prob_1
-            self.ratings[comp_1] += update
-            self.ratings[comp_2] -= update
+            a_1, b_1, a_2, b_2 = self.get_coefs(idx_1=comp_1, idx_2=comp_2)
+            s_1 = (outcomes[idx] - 0.5) * 2.0
+            s_2 = -1.0 * s_1
+            r_1_new = (a_1 * r_1) + (b_1 * r_2) + (b_1 * s_1)
+            r_2_new = (a_2 * r_2) + (b_2 * r_1) + (b_2 * s_2)
+            self.ratings[comp_1] = r_1_new
+            self.ratings[comp_2] = r_2_new
 
     def print_top_k(self, k, competitor_names):
         sorted_idxs = np.argsort(-self.ratings)[:k]
