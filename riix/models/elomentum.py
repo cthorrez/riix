ba41a7a12a32d7e1bf0b0535de8354fba91dd1c7
cthorrez <claytonthorrez@gmail.com>
2024-01-05 21:35:47 -0800

    add elomentum

---
+++ riix/models/elomentum.py
@@ -0,0 +1,90 @@
+"""
+Elo + Momentum
+Invented by Clayton Thorrez (me)
+"""
+import math
+import numpy as np
+from riix.core.base import OnlineRatingSystem
+from riix.utils.math_utils import sigmoid
+
+
+class EloMentum(OnlineRatingSystem):
+    def __init__(
+        self,
+        num_competitors: int,
+        initial_rating: float = 1500.0,
+        k: float = 32.0,
+        alpha: float = math.log(10.0) / 400.0,
+        momentum: float = 0.3,
+        update_method: str = 'iterative',
+        dtype=np.float64,
+    ):
+        self.num_competitors = num_competitors
+        self.k = k
+        self.alpha = alpha
+        self.momentum = momentum
+        self.ratings = np.zeros(shape=num_competitors, dtype=dtype) + initial_rating
+        self.v = np.zeros(shape=num_competitors, dtype=dtype)
+        self.cache = {'probs': None}
+        if update_method == 'batched':
+            self.update_fn = self.batched_update
+        elif update_method == 'iterative':
+            self.update_fn = self.iterative_update
+
+    def predict(self, time_step: int, matchups: np.ndarray, set_cache: bool = False):
+        """generate predictions"""
+        ratings_1 = self.ratings[matchups[:, 0]]
+        ratings_2 = self.ratings[matchups[:, 1]]
+        probs = sigmoid(self.alpha * (ratings_1 - ratings_2))
+        if set_cache:
+            self.cache['probs'] = probs
+        return probs
+
+    def fit(
+        self,
+        time_step: int,
+        matchups: np.ndarray,
+        outcomes: np.ndarray,
+        use_cache: bool = False,
+    ):
+        self.update_fn(matchups, outcomes, use_cache=use_cache)
+
+    def batched_update(self, matchups, outcomes, use_cache):
+        """apply one update based on all of the results of the rating period"""
+        pass
+        # active_in_period = np.unique(matchups)
+        # masks = np.equal(matchups[:, :, None], active_in_period[None, :])  # N x 2 x active
+        # if use_cache:
+        #     probs = self.cache['probs']
+        # else:
+        #     probs = self.predict(time_step=None, matchups=matchups, set_cache=False)
+        # per_match_diff = (outcomes - probs)[:, None]
+        # per_match_diff = np.hstack([per_match_diff, -per_match_diff])
+        # per_competitor_diff = (per_match_diff[:, :, None] * masks).sum(axis=(0, 1))
+        # self.ratings[active_in_period] += self.k * per_competitor_diff
+
+    def iterative_update(self, matchups, outcomes, **kwargs):
+        """treat the matchups in the rating period as if they were sequential"""
+        for idx in range(matchups.shape[0]):
+            comp_1, comp_2 = matchups[idx]
+            diff = self.ratings[comp_1] - self.ratings[comp_2]
+            v_1 = self.v[comp_1]
+            v_2 = self.v[comp_2]
+            prob = sigmoid(self.alpha * diff)
+            g_1 = (
+                outcomes[idx] - prob
+            )  # I think technically this is not the gradient because of alpha but it's close enough
+            g_2 = -1.0 * g_1
+            v_1_new = (self.momentum * v_1) + g_1
+            v_2_new = (self.momentum * v_2) + g_2
+
+            self.ratings[comp_1] += self.k * v_1_new
+            self.ratings[comp_2] += self.k * v_2_new
+            self.v[comp_1] = v_1_new
+            self.v[comp_2] = v_2_new
+
+    def print_top_k(self, k, competitor_names):
+        sorted_idxs = np.argsort(-self.ratings)[:k]
+        for k_idx in range(k):
+            comp_idx = sorted_idxs[k_idx]
+            print(competitor_names[comp_idx], self.ratings[comp_idx])
