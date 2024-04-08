6df4729e261dc8f905f3b57d5b8483ec73b544df
cthorrez <claytonthorrez@gmail.com>
2024-04-07 17:17:10 -0700

    rework grid search configuration

--- riix/eval.py
+++ riix/eval.py
@@ -2,7 +2,6 @@
 import time
 from functools import partial
 from copy import deepcopy
-from itertools import product
 from multiprocessing import Pool
 import numpy as np
 from riix.core.base import OnlineRatingSystem
@@ -31,35 +30,11 @@ def eval_wrapper(params, rating_system_class, dataset, test_dataset=None):
     return evaluate(model, dataset, test_dataset=test_dataset)
 
 
-def create_uniform_grid(
-    param_ranges,
-    num_samples,
-    seed=0,
-):
-    rng = np.random.default_rng(seed)
-    num_hyperparams = len(param_ranges)
-    low = np.empty(num_hyperparams)
-    high = np.empty(num_hyperparams)
-    for idx, param_range in enumerate(param_ranges.values()):
-        low[idx] = param_range[0]
-        high[idx] = param_range[1]
-    values = rng.uniform(low=low, high=high, size=(num_samples, num_hyperparams))
-    params = []
-    for sample_idx, values_row in enumerate(values):
-        current_params = {}
-        for param_idx, param_name in enumerate(param_ranges.keys()):
-            current_params[param_name] = values_row[param_idx]
-        params.append(current_params)
-    return params
-
-
 def grid_search(
     rating_system_class,
     dataset,
+    param_configurations=None,
     test_dataset=None,
-    param_grid=None,
-    param_ranges=None,
-    num_samples=None,
     metric='log_loss',
     minimize_metric=True,
     num_processes=None,
@@ -77,26 +52,17 @@ def grid_search(
     metric_multiplier = 1.0 if minimize_metric else -1.0
     best_metric = np.inf
 
-    if (param_grid is not None) and (param_ranges is None):
-        all_params = product(*param_grid.values())
-        inputs = [dict(zip(param_grid.keys(), params)) for params in all_params]
-    elif (param_ranges is not None) and (param_grid is None):
-        inputs = create_uniform_grid(param_ranges, num_samples, seed)
-        pass
-    else:
-        raise ValueError('you must only specify one of param_grid and param_ranges')
-
     func = partial(
         eval_wrapper,
         rating_system_class=rating_system_class,
         dataset=dataset,
     )
-    all_metrics = list(map_fn(func, inputs))
+    all_metrics = list(map_fn(func, param_configurations))
     if num_processes:
         pool.close()
         pool.join()
 
-    for current_params, current_metrics in zip(inputs, all_metrics):
+    for current_params, current_metrics in zip(param_configurations, all_metrics):
         current_metric = current_metrics[metric]
         # Compare and update best metric and params
         if current_metric * metric_multiplier < best_metric:
