2914abedd4426311c71f930d43d4e15f56115923
cthorrez <claytonthorrez@gmail.com>
2024-04-17 21:03:58 -0700

    add sigma update to glicko2

--- riix/models/glicko2.py
+++ riix/models/glicko2.py
@@ -21,20 +21,24 @@ class Glicko2(OnlineRatingSystem):
     def __init__(
         self,
         competitors: list,
-        initial_rating: float = 0.0,  # I'm choosing the natural scale to avoid ocnverting back and forth
-        initial_phi: float = 2.0147,
+        initial_rating: float = 1500.0,
+        initial_rd: float = 350.0,
         initial_sigma: float = 0.06,
-        tau: float = 0.2,
+        tau: float = 0.5,
+        epsilon: float = 1e-6,
         dtype=np.float64,
         update_method='iterative',
     ):
         """Initializes the Glicko rating system with the given parameters."""
         super().__init__(competitors)
-        self.mus = np.zeros(shape=self.num_competitors, dtype=dtype) + initial_rating
-        self.initial_phi = initial_phi
-        self.phis = np.zeros(shape=self.num_competitors, dtype=dtype) + initial_phi
+        self.mus = np.zeros(shape=self.num_competitors, dtype=dtype) + ((initial_rating - 1500.0) / 173.7178)
+        self.initial_phi = initial_rd / 173.7178
+        self.phis = np.zeros(shape=self.num_competitors, dtype=dtype) + self.initial_phi
         self.sigmas = np.zeros(shape=self.num_competitors, dtype=dtype) + initial_sigma
         self.has_played = np.zeros(shape=self.num_competitors, dtype=np.bool_)
+        self.tau = tau
+        self.tau2 = tau**2.0
+        self.epsilon = epsilon
 
         if update_method == 'batched':
             self.update = self.batched_update
@@ -56,14 +60,14 @@ class Glicko2(OnlineRatingSystem):
         """generate predictions"""
         mu_1 = self.mus[matchups[:, 0]]
         mu_2 = self.mus[matchups[:, 1]]
-        rating_diffs = mu_1 - mu_2
+        mu_diff = mu_1 - mu_2
         phi_1 = self.phis[matchups[:, 0]]
         phi_2 = self.phis[matchups[:, 1]]
         # the papers and theory seem to indicate this...
-        # combined_dev = self.g_vector(np.sqrt(np.square(phi_1) + np.square(phi_2)))
+        # combined_phi = self.g_vector(np.sqrt(np.square(phi_1) + np.square(phi_2)))
         # but this seems to work better...
-        combined_dev = self.g_vector(phi_1 + phi_2)
-        probs = sigmoid(combined_dev * rating_diffs)
+        combined_phi = self.g_vector(phi_1 + phi_2)
+        probs = sigmoid(combined_phi * mu_diff)
         return probs
 
     def get_pre_match_ratings(self, matchups: np.ndarray, **kwargs):
@@ -85,8 +89,39 @@ class Glicko2(OnlineRatingSystem):
         """apply one update based on all of the results of the rating period"""
         # TODO
 
-    def get_sigma_star(self, sigma):
-        return sigma
+    def f(self, x, delta2, phi2, v, a):
+        ex = math.exp(x)
+        phi2_v_ex = phi2 + v + ex
+        num_1 = ex * (delta2 - phi2_v_ex)
+        denom_1 = 2 * ((phi2_v_ex) ** 2.0)
+        term_2 = (x - a) / self.tau2
+        return (num_1 / denom_1) - term_2
+
+    def get_sigma_star(self, phi, delta, v, sigma):
+        delta2 = delta**2.0
+        phi2 = phi**2.0
+        A = a = math.log(sigma**2.0)
+        if delta2 > (phi2 + v):
+            B = math.log(delta2 - phi2 - v)
+        else:
+            B = a - self.tau
+            while self.f(B, delta2=delta2, phi2=phi2, v=v, a=a) < 0:
+                B -= self.tau
+
+        f_A = self.f(A, delta2, phi2, v, a)
+        f_B = self.f(B, delta2, phi2, v, a)
+        while math.fabs(A - B) > self.epsilon:
+            C = A + ((A - B) * f_A) / (f_B - f_A)
+            f_C = self.f(C, delta2, phi2, v, a)
+            if (f_C * f_B) <= 0:
+                A = B
+                f_A = f_B
+            else:
+                f_A = f_A / 2.0
+            B = C
+            f_B = f_C
+        sigma_prime = math.exp(A / 2.0)
+        return sigma_prime
 
     def iterative_update(self, matchups, outcomes, **kwargs):
         """treat the matchups in the rating period as if they were sequential"""
@@ -95,26 +130,31 @@ class Glicko2(OnlineRatingSystem):
             comp_1, comp_2 = matchups[idx]
             mu_1 = self.mus[comp_1]
             mu_2 = self.mus[comp_2]
-            rating_diff = mu_1 - mu_2
+            mu_diff = mu_1 - mu_2
             phi_1 = self.phis[comp_1]
             phi_2 = self.phis[comp_2]
             g_1 = self.g_scalar(phi_1)
             g_2 = self.g_scalar(phi_2)
-            p_1 = sigmoid_scalar(g_1 * rating_diff)
-            p_2 = sigmoid_scalar(-g_2 * rating_diff)
+            p_1 = sigmoid_scalar(g_1 * mu_diff)
+            p_2 = sigmoid_scalar(-g_2 * mu_diff)
             v_1 = 1.0 / ((g_1**2.0) * p_1 * (1.0 - p_1))
             v_2 = 1.0 / ((g_2**2.0) * p_2 * (1.0 - p_2))
-            # delta_1 = v_1 * g_1 * (outcomes[idx] - p_1)
-            # delta_2 = v_2 * g_2 * (1.0 - outcomes[idx] - p_2)
 
-            # sigma_star_1 = self.get_sigma_star(self.sigmas[comp_1])
-            # sigma_star_2 = self.get_sigma_star(self.sigmas[comp_2])
+            delta_1 = v_1 * g_1 * (outcomes[idx] - p_1)
+            delta_2 = v_2 * g_2 * (1.0 - outcomes[idx] - p_2)
+
+            sigma_star_1 = self.get_sigma_star(phi_1, delta_1, v_1, self.sigmas[comp_1])
+            sigma_star_2 = self.get_sigma_star(phi_2, delta_2, v_2, self.sigmas[comp_2])
+
+            self.sigmas[comp_1] = sigma_star_1
+            self.sigmas[comp_2] = sigma_star_2
 
             # I guess don't do this since I update them all at the beginning?
-            # phi_star_1 = (self.phis[comp_1] ** 2.0) + (sigma_star_1 ** 2.0)
-            # phi_star_2 = (self.phis[comp_2] ** 2.0) + (sigma_star_2 ** 2.0)
-            phi_star_1 = self.phis[comp_1] ** 2.0
-            phi_star_2 = self.phis[comp_2] ** 2.0
+            phi_star_1 = (self.phis[comp_1] ** 2.0) + (sigma_star_1**2.0)
+            phi_star_2 = (self.phis[comp_2] ** 2.0) + (sigma_star_2**2.0)
+
+            # phi_star_1 = self.phis[comp_1] ** 2.0
+            # phi_star_2 = self.phis[comp_2] ** 2.0
 
             self.phis[comp_1] = 1.0 / math.sqrt((1.0 / phi_star_1) + (1.0 / (v_1**2.0)))
             self.phis[comp_2] = 1.0 / math.sqrt((1.0 / phi_star_2) + (1.0 / (v_2**2.0)))
